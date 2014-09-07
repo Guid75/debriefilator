c2bf161c9d7e61c349695659c4ba6cc87a6152a4
Guillaume Denry <guillaume.denry@gmail.com>
2014-09-07 16:15:05 +0200

    Add retro & note controllers/models

---
+++ api/controllers/RetroController.js
@@ -0,0 +1,11 @@
+/**
+ * RetroController
+ *
+ * @description :: Server-side logic for managing retrospective sessions
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Note.js
@@ -0,0 +1,22 @@
+/**
+* Note.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+	attributes: {
+		text: {
+			type: 'string'
+		},
+		score: {
+			type: 'integer',
+			defaultsTo: 0
+		},
+        owner: {
+            model: 'retro'
+        }
+    }
+};
+
